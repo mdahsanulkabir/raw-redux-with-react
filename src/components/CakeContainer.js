e55c5edd175bebe79f3f4f6771b92fa6f419ec03
ahsan <makabir98@yahoo.com>
2023-02-07 07:30:22 +0600

    cake component added

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,11 @@
+function CakeContainer() {
+    return (
+        <div>
+            <h2>Number of Cakes</h2>
+            <button>Buy Cake</button>
+        </div>
+    )
+
+}
+
+export default CakeContainer;
